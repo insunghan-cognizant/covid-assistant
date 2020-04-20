407f1c84ec7df3375842e42b38001c774631ec4d
Insung Han <insung.han@fintivtech.com>
2020-04-20 14:27:32 -0400

    add cases in county

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -399,6 +399,24 @@ var ConversationPanel = (function () {
           textToDisplay += county +': ' + cases + ' cases, ' + deaths + ' deaths, ' + recovered + ' recovered, as of ' + date + '<hr/>'
      
         } 
+        
+      } else if( parsedObj.type == "cases-county") {
+        var state = parsedObj.state;
+        if( state.length <= 2 ) {
+          state = abbrState(state, "name")
+        }
+        for (var i = 0; i < parsedObj.summary.data.length; i++) {
+          var id = parsedObj.summary.data[i].id;
+          var county = parsedObj.summary.data[i].name
+          var cases = parsedObj.summary.data[i].latestData.cases
+          var deaths = parsedObj.summary.data[i].latestData.deaths
+          var recovered = parsedObj.summary.data[i].latestData.recovered
+          var date = parsedObj.summary.data[i].latestData.date
+        
+          if( id.includes(state.toLowerCase()) ) {
+            textToDisplay = cases + ' cases, ' + deaths + ' deaths, ' + recovered + ' recovered, as of ' + date 
+          }
+        } 
       
       }
 
@@ -408,6 +426,78 @@ var ConversationPanel = (function () {
     return textToDisplay;
   }
 
+
+  function abbrState(input, to){
+    
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    if (to == 'abbr'){
+        input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        for(i = 0; i < states.length; i++){
+            if(states[i][0] == input){
+                return(states[i][1]);
+            }
+        }    
+    } else if (to == 'name'){
+        input = input.toUpperCase();
+        for(i = 0; i < states.length; i++){
+            if(states[i][1] == input){
+                return(states[i][0]);
+            }
+        }    
+    }
+}
 
   // Constructs new generic elements from a message payload
   function buildMessageDomElements(newPayload, isUser) {
